dc3bb79661fed03437b21283ddf9f5f921473701
MJ Dev <mjdev.team22@gmail.com>
2022-08-02 09:56:53 +0700

    pertama

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import {ref} from 'vue'
+  const message = ref("halo dari vuejs 3")
+
+  setTimeout(() => {
+    console.log(message)
+    message.value = "saya fajar siagian"
+  }, 5000);
+</script>
+
+<template>
+  <div>{{message}}</div>
+</template>
+
+<style lang="css" scoped>
+</style>
